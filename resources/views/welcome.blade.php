957e727b903f2b3a1b2c99b07c9534c731132efa
Unknown <arturodlt83@gmail.com>
2018-03-25 12:21:09 -0600

    cambio a libreria de web

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+        <!-- <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+        <script src="/componentes/jquery/dist/jquery.min.js"></script>
+        <script src="/bootstrap/js/bootstrap.min.js"></script> -->
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
         <title>Laravel</title>
 
@@ -56,9 +62,6 @@
           </a>
         </div>
       </div>
-
-      <script src="/componentes/jquery/dist/jquery.min.js"></script>
-      <script src="/bootstrap/js/bootstrap.min.js"></script>
 
     </body>
 </html>
