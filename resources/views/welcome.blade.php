47954e03f14bb228448acfc1f5c55677ed946303
Unknown <arturodlt83@gmail.com>
2018-03-25 14:44:39 -0600

    Revert "se retira un slide del carrusel"
    This reverts commit 4e5d95dfd94db7ae6c65c37145efd28df54daf17.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,11 +41,11 @@
           <!-- Wrapper for slides -->
           <div class="carousel-inner">
             <div class="item active">
-              <img src="/img/IMG_20171121_113729_098.jpg" alt="Los Angeles" style="width:50%;">
+              <img src="/img/IMG_20171121_113729_098.jpg" alt="Los Angeles" style="width:100%;">
             </div>
 
             <div class="item">
-              <img src="/img/red.png" alt="Chicago" style="width:50%;">
+              <img src="/img/red.png" alt="Chicago" style="width:100%;">
             </div>
 
           </div>
@@ -53,7 +53,7 @@
           <!-- Left and right controls -->
           <a class="left carousel-control" href="#myCarousel" data-slide="prev">
             <span class="glyphicon glyphicon-chevron-left"></span>
-            <span class="sr-only"></span>
+            <span class="sr-only"><</span>
           </a>
 
           <a class="right carousel-control" href="#myCarousel" data-slide="next">
@@ -62,6 +62,5 @@
           </a>
         </div>
       </div>
-
     </body>
 </html>
