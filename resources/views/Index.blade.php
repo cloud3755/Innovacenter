2f46ffdd6705f71fab46abb7ac285ef16d659447
Unknown <arturodlt83@gmail.com>
2018-03-31 13:12:52 -0600

    se agrega animacion al formulario

--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -393,13 +393,15 @@
                     $('#ocultar').hide();
 
                     $("#btnquejas").on( "click", function() {
-                      $('#ocultar').show();
+                      $('#ocultar').fadeOut();
+                      $('#ocultar').fadeIn(4000);
                       $('#tipomensaje').html('Queja')
                      });
 
                    $("#btncontacto").on( "click", function() {
 
-                     $('#ocultar').show();
+                     $('#ocultar').fadeOut();
+                     $('#ocultar').fadeIn(4000);
 
                      $('#tipomensaje').html('Contactanos')
                     });
