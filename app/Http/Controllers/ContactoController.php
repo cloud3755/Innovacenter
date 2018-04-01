f2351e0aac2d95d25d20c83022e832a49d7edf4d
Unknown <arturodlt83@gmail.com>
2018-03-31 23:04:03 -0500

    se modifica para que la calse este configurada, hace falta agregar los parametros a la clase

--- app/Http/Controllers/ContactoController.php
+++ app/Http/Controllers/ContactoController.php
@@ -13,23 +13,18 @@ class ContactoController extends Controller
     public function nuevo(Request $request)
     {
 
-      Mail::to('ilemiranda1@gmail.com','Arturo')
-              ->send(new ConcactoMail());
 
         $contact =  new Contacto;
         $contact->Nombre = $request->nombre;
         $contact->Correo = $request->correo;
         $contact->Telefono = $request->telefono;
         $contact->Mensaje = $request->mensaje;
-        // $contact->FechaHoraCreacion = date('Y-m-d'). 'T' .date('H:i:s');
+        $contact->FechaHoraCreacion = date('Y-m-d'). 'T' .date('H:i:s');
         $contact->save();
 
+        Mail::to('ilemiranda1@gmail.com','Arturo')
+                ->send(new ConcactoMail());
 
-        // $data['foo'] = 'bar';
-        // Mail::send('layouts.template', $data, function($message)
-        // {
-        //     $message->to('ilemiranda1@gmail.com', 'Jon Doe')->subject('INTERESADO');
-        // });
 
         return response()->json("OK");
     }
