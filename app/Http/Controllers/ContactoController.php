7eaff6633b866de0f03f647824034c450db1572f
Unknown <arturodlt83@gmail.com>
2018-03-31 18:33:04 -0600

    se cre vista de html  y se agrega metodo para enviar correo

--- app/Http/Controllers/ContactoController.php
+++ app/Http/Controllers/ContactoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Contacto;
+use Mail;
 
 class ContactoController extends Controller
 {
@@ -17,6 +18,14 @@ class ContactoController extends Controller
         $contact->Correo = $request->correo;
         $contact->FechaHoraCreacion = date('Y-m-d'). 'T' .date('H:i:s');
         $contact->save();
+
+        $data['foo'] = 'bar';
+
+        Mail::send('layouts.template', $data, function($message)
+        {
+            $message->to('ilemiranda1@gmail.com', 'Jon Doe')->subject('INTERESADO');
+        });
+
         return response()->json("OK");
     }
 }
