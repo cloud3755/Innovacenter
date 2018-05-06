aa4ba8ac076fa7fa115805819fc44b39d6461e0d
Unknown <arturodlt83@gmail.com>
2018-05-06 18:09:18 -0500

    se agrega para cargar a servidor

--- config/database.php
+++ config/database.php
@@ -1,5 +1,10 @@
 <?php
 
+define ('RDS_HOSTNAME',$SERVER['RDS_HOSTNAME']);
+define ('RDS_USERNAME',$SERVER['RDS_USERNAME']);
+define ('RDS_PASSWORD',$SERVER['RDS_PASSWORD']);
+define ('RDS_DB_NAME',$SERVER['RDS_DB_NAME']);
+
 return [
 
     /*
@@ -39,13 +44,28 @@ return [
             'prefix' => '',
         ],
 
+        // 'mysql' => [
+        //     'driver' => 'mysql',
+        //     'host' => env('DB_HOST', '127.0.0.1'),
+        //     'port' => env('DB_PORT', '3306'),
+        //     'database' => env('DB_DATABASE', 'innovacenter'),
+        //     'username' => env('DB_USERNAME', 'root'),
+        //     'password' => env('DB_PASSWORD', ''),
+        //     'unix_socket' => env('DB_SOCKET', ''),
+        //     'charset' => 'utf8mb4',
+        //     'collation' => 'utf8mb4_unicode_ci',
+        //     'prefix' => '',
+        //     'strict' => true,
+        //     'engine' => null,
+        // ],
+
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => RDS_HOSTNAME,
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'innovacenter'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => RDS_DB_NAME,
+            'username' => RDS_USERNAME,
+            'password' => RDS_PASSWORD,
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
